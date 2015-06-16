b429fa3866a1dcde6798a02ecea02810512a4bab
Kelwin Gamez <kelgwiin@gmail.com>
2015-06-16 09:46:31 -0430

    Added and Modified Main Class showing that a class cannot inherite from a class with a default access.

---
+++ SCJP_2015/src/main/Main.java
@@ -0,0 +1,16 @@
+package main;
+
+import test1.*;
+/**
+ * 1) We cannot extends from a class that has a package access
+ * @author kgamez
+ *
+ */
+public class Main /**1) extends Coo1**/{
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
