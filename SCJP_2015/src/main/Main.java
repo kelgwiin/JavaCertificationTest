2f3e7164f44a7bb4ede4f2822aac73c3b3a4e4e7
Kelwin Gamez <kelgwiin@gmail.com>
2015-06-16 10:54:36 -0430

    Inner class within an abstract

--- SCJP_2015/src/main/Main.java
+++ SCJP_2015/src/main/Main.java
@@ -1,12 +1,16 @@
 package main;
 
 import test1.*;
+import test1.IllegalClassAbstract.*;
+
 /**
  * 1) We cannot extends from a class that has a package access
+ * 
  * @author kgamez
- *
+ * 
  */
-public class Main /**1) extends Coo1**/{
+public class Main /** 1) extends Coo1 **/
+{
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
@@ -16,16 +20,17 @@ public class Main /**1) extends Coo1**/{
 				// TODO Auto-generated method stub
 				System.out.println("body method @myMethod");
 			}
-			
+
 			/**
 			 * This has error because it has a modifier final inside.
 			 */
-//			public final void myMethodFinalIllegal(){
-//				System.out.println("Final Method inside an Abstract Class");
-//			}
-			
-			
+			// public final void myMethodFinalIllegal(){
+			// System.out.println("Final Method inside an Abstract Class");
+			// }
+
 		};
+		InnerPublicClass pc =  ac.new InnerPublicClass();
+		
 		ac.myMethod();
 		ac.myMethodFinalIllegal();
 
--- SCJP_2015/src/test1/IllegalClassAbstract.java
+++ SCJP_2015/src/test1/IllegalClassAbstract.java
@@ -2,12 +2,29 @@ package test1;
 
 public abstract class IllegalClassAbstract {
 	public abstract void myMethod();
-	
+
 	/**
 	 * This method seems Illegal but isn't
 	 */
-	public final void myMethodFinalIllegal(){
+	public final void myMethodFinalIllegal() {
 		System.out.println("Final Method inside an Abstract Class");
+		InnerClass ic = new InnerClass();
+		ic.print();
 	}
 
+	private class InnerClass {
+		public void print(){
+			System.out.println("Private Nested Class");
+		}
+	}
+	
+	public class InnerPublicClass {
+		
+		public InnerPublicClass(){
+			
+		}
+		public void print(){
+			System.out.println("Public Nested Class");
+		}
+	}
 }
