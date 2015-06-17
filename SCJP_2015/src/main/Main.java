3a05e9522844af072121a45084cb3a544fcf9b29
Kelwin Gamez <kelgwiin@gmail.com>
2015-06-17 10:09:58 -0430

    Examples of Custom Enums

--- SCJP_2015/src/main/Main.java
+++ SCJP_2015/src/main/Main.java
@@ -11,8 +11,44 @@ import test1.IllegalClassAbstract.*;
  */
 public class Main /** 1) extends Coo1 **/
 {
-
+	enum MyEnum {
+		UNO(10){
+			public String getName(){
+				return "Uno ";
+			}
+		}, 
+		DOS(20) ,
+		TRES(30);
+		
+		private int num;
+		private String name;
+		MyEnum(int num){
+			this.num = num;
+			name = "Número";
+		}
+		
+		public int getNum(){
+			return num;
+		}
+		
+		public String getName(){
+			return name;
+		}
+	}
+	
 	public static void main(String[] args) {
+		MyEnum en;
+		en = MyEnum.DOS;
+		Main.print(en.getNum());
+		Main.print(en.getName());
+		//Main.test1();
+		
+	}
+	
+	public static void print(Object obj){
+		System.out.println(obj);
+	}
+	public static void test1() {
 		// TODO Auto-generated method stub
 		IllegalClassAbstract ac = new IllegalClassAbstract() {
 			@Override
@@ -29,8 +65,8 @@ public class Main /** 1) extends Coo1 **/
 			// }
 
 		};
-		InnerPublicClass pc =  ac.new InnerPublicClass();
-		
+		InnerPublicClass pc = ac.new InnerPublicClass();
+
 		ac.myMethod();
 		ac.myMethodFinalIllegal();
 
